8954bf5c4423222fe5ffdecab9e99bd834b001a0
tivereidoro <tivereidoro@yahoo.com>
2025-03-19 12:45:22 +0100

    Init: Setup new project

---
+++ app/global-error.tsx
@@ -0,0 +1,5 @@
+"use client";
+
+export default function globalError() {
+  return <div>Error: Something went wrong</div>;
+}
---
+++ app/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div>
+      <p>Loading content ...</p>
+    </div>
+  );
+}
